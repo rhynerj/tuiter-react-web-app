010137abc9575c850fc7d88bd12a8ed99b169ad9
rhynerj <rhyner.j@husky.neu.edu>
2023-07-25 19:16:13 -0700

    a4 State and Redux labs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <HashRouter>
       <div className="container-fluid m-0 pt-3 pb-3 w-100">
         <Routes>
-          <Route path="/" element={<Navigate to="/labs"/>}/>
+          <Route path="/" element={<Navigate to="/labs/a3"/>}/>
           <Route path="/labs/*" element={<Labs />} />
           <Route path="/hello" element={<HelloWorld />} />
           <Route path="/tuiter/*" element={<Tuiter />} />
